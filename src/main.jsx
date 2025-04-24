42aa5d9f2e15464195610e1553ca6e2aac4c0fb6
salih-sengul <salih.sengul@outlook.com>
2025-04-24 13:36:50 +0300

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <HashRouter
+  basename={"/Context-Api-Shopping-Cart"}
+>
+  <App />
+</HashRouter>
 );
