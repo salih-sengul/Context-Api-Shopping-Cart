d930aa5d4e6907fb6dfbc0942872221f6f32cf44
salih-sengul <salih.sengul@outlook.com>
2025-04-24 13:48:44 +0300

    BrowserRouter back to

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
-
 import './index.css';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter
-  basename={"/Context-Api-Shopping-Cart"}
->
+<BrowserRouter basename={"/Context-Api-Shopping-Cart"}>
   <App />
-</HashRouter>
+</BrowserRouter>
 );
