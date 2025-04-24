54fa7845ab04b7219f83d79f6014eb2efc6f47ee
salih-sengul <salih.sengul@outlook.com>
2025-04-24 13:17:31 +0300

    publish

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:"/Context-Api-Shopping-Cart",
+  base:"/Context-Api-Shopping-Cart/",
   test: {
     globals: true,
     environment: 'jsdom',
